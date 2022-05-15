2ff14c6d7bb4c7d24fae432e4171b111314d33e9
bearomorphism <bear890707@gmail.com>
2022-05-15 20:31:39 +0800

    remove answer

--- xkcd-api-practice.py
+++ xkcd-api-practice.py
@@ -4,24 +4,14 @@ practice:
 
 下載 xkcd 前十張圖片 (0~9)，檔名格式為 <編號>-<檔案名稱>
     example: 614-woodpecker.png
+有些編號其實會拿到 404 not found 請注意
 '''
 
 import requests
 from pathlib import Path
 Path("./images").mkdir(parents=True, exist_ok=True)
 
-
-for i in range(10):
-    url = f'https://xkcd.com/{i}/info.0.json'
-    res = requests.get(url)
-    if res.ok:
-        res_json = res.json()
-        img_url = res_json['img']
-
-        img_name = img_url.split('/')[-1]
-        with open(Path(f"./images/{i}-{img_name}"), "wb") as f:
-            img_res = requests.get(img_url)
-            f.write(img_res.content)
+# 請開始你的練習
 
 
 '''
